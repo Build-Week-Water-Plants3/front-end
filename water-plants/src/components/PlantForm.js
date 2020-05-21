45c95d7718d9731c654c5ab5f65dcceb74188f5d
triggering-event <gabriel.j.shapiro@gmail.com>
2020-05-21 19:33:27 -0400

    creates PlantForm component

--- water-plants/src/components/PlantForm.js
+++ water-plants/src/components/PlantForm.js
@@ -1,3 +1,13 @@
 import React, {useState, useEffect} from 'react';
+import * as yup from 'yup';
 
+const Form = (props) => {
+    return (
+        <form action="">
+            <input type="text">Name of Plant:</input>
+        </form>
+    )
+}
+
+export default Form;
 
