b78b8ae99505c34f7d10340d55f1288bb990e94a
triggering-event <gabriel.j.shapiro@gmail.com>
2020-05-21 20:33:03 -0400

    adds comments

--- water-plants/src/components/PlantForm.js
+++ water-plants/src/components/PlantForm.js
@@ -1,8 +1,18 @@
+/***************
+ LIBRARIES
+ ***************/
 import React, {useState, useEffect} from 'react';
 import * as yup from 'yup';
+
+/***************
+ COMPONENTS/DATA
+ ***************/
 import dummyPlants from "../dummydata";
 
 const PlantForm = (props) => {
+    /***************
+     HOOKS
+     ***************/
     const [plants, setPlants] = useState(dummyPlants);
     const blankPlant = {
         id: plants.length + 1,
@@ -13,6 +23,9 @@ const PlantForm = (props) => {
     }
     const [newPlant, setNewPlant] = useState(blankPlant);
 
+    /***************
+     EVENT HANDLERS
+     ***************/
     const handleChange = (event) => {
         setNewPlant({...newPlant, [event.target.name]: event.target.value})
     }
@@ -22,13 +35,16 @@ const PlantForm = (props) => {
         setNewPlant(blankPlant);
     }
 
+    /***************
+     DISPLAY
+     ***************/
     return (
         <form style={{display: 'flex', flexDirection: 'column'}} onSubmit={handleSubmit}>
             <h1>Add a New Plant!</h1>
             <div>Nickname: <input type='text' name={'nickname'} value={newPlant.nickname} onChange={handleChange}/>
             </div>
             <div>Species: <input type='text' name={'species'} value={newPlant.species} onChange={handleChange}/></div>
-            {/*note to self: change water frequency selection to drop down menus*/}
+            {/*note to self: change water frequency selection to dropdown menus*/}
             <div>How Often to Water: <input type='text' name={'h2oFrequency'} value={newPlant.h2oFrequency}
                                             onChange={handleChange}/></div>
             <div>Image (URL): <input type="url" name={'image'} value={newPlant.image} onChange={handleChange}/></div>
@@ -37,4 +53,7 @@ const PlantForm = (props) => {
     )
 }
 
+/***************
+ EXPORTS
+ ***************/
 export default PlantForm;
