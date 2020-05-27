e3233ca179d0a302216bf1573b8825af270ecf16
triggering-event <gabriel.j.shapiro@gmail.com>
2020-05-26 20:43:13 -0400

    suggests changes to Ignacio's component and adds routing

--- water-plants/src/App.js
+++ water-plants/src/App.js
@@ -1,19 +1,36 @@
-
 import React from "react";
-import Plants from './components/Plants'
-import { BrowserRouter as  Route}  from "react-router-dom";
-
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+import PlantsDisplay from './components/PlantsDisplay';
+import PlantForm from './components/PlantForm';
 
 function App() {
+    return (
+        <Router>
+            <nav className="navbar">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/plantform">Add a New Plant</Link>
+                </li>
+                <li>
+                    <Link to="/plantsdisplay">Display Your Current Plants</Link>
+                </li>
+            </nav>
+            <div className="App">
+                <Switch>
+                        <Route path="/plantsdisplay">
+                            <PlantsDisplay/>
+                        </Route>
+                        <Route path="/plantform">
+                            <PlantForm/>
+                        </Route>
 
-return(
-    <div className="App">
-   <Route path="/plants" component={Plants}>
-    <Plants></Plants>
-  </Route>
-    </div>
-)
-  }
+                </Switch>
+            </div>
+        </Router>
+    )
+}
 
 
 export default App;
--- water-plants/src/components/PlantsDisplay.js
+++ water-plants/src/components/PlantsDisplay.js
@@ -1,23 +1,24 @@
-
 import React from 'react'
+import dummyPlants from "../dummydata";
+import axios from 'axios';
 
 
-const PlantSubmision = props => {
-  return (
-    <div className="new-plant">
-      {props.newPlant.map(plant => (
-        <div className="plants"   >
-          <h2>{plant.nickname}</h2>
-          <p>{plant.species}</p>
-          <p>{plant.h2oFrequency}</p>
-          <p>{plant.image}</p>
-       
-     
-          
+const PlantsDisplay = props => {
+    return (
+        <div style={{borderStyle: "solid", margin: '20px'}}>
+            <h2>Your Plants:</h2>
+            {dummyPlants.map((plant) =>
+                <ul>
+                    <li>ID: {plant.id}</li>
+                    <li>Nickname: {plant.nickname} </li>
+                    <li>Species: {plant.species} </li>
+                    <li>Water {plant.h2oFrequencyTimes} times per {plant.h2oFrequencyPeriod}</li>
+                    <li>Image: <img src={plant.image}/></li>
+                </ul>
+            )}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
-export default PlantSubmision
+export default PlantsDisplay;
+
